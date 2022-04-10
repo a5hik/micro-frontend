a1969ba7f8e2eff1803fc298881eb1d75b8aae1b
Muhammed Ashik <mashik@arubanetworks.com>
2022-04-10 17:27:38 +0530

    micro frontend proj init.

---
+++ cart/webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const { plugins } = require('../container/webpack.config');
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        port: 8083
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'cart',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './CartIndex' : './src/bootstrap'
+            },
+            shared: ['faker']
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
---
+++ container/src/bootstrap.js
@@ -0,0 +1,5 @@
+import {mount as productsMount} from 'products/ProductsIndex';
+import {mount as cartMount} from 'cart/CartIndex';
+
+productsMount(document.querySelector('#my-products'));
+cartMount(document.querySelector('#my-cart'));
